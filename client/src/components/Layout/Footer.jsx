9e4d66faec06ae1742134e39ab72393e398f31aa
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-09 20:49:31 +0530

    :zap: :art: :wrench: Routes added with layout

---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">All Rights Reserved &copy; Vasant</h4>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout/Layout"
+
+const Homepage = () => {
+  return (
+    <Layout>
+      <h1>Home</h1>
+    </Layout>
+  )
+}
+
+export default Homepage
