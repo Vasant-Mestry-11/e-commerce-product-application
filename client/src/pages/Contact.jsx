9b0535901f22a6a78c38cd8122e4c16149271503
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-11 18:11:51 +0530

    :art: :lipstick: Contact Us page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,37 @@
 import Layout from "../components/Layout/Layout";
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { FiPhoneCall } from "react-icons/fi";
+import { MdHeadsetMic } from "react-icons/md";
 
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact</h1>
+      <div className="contact-container">
+        <div className="left-panel">
+          <img src="/images/contactus.jpeg" alt="contact us" />
+        </div>
+        <div className="right-panel">
+          <button className="contact-button">Contact Us</button>
+          <p className="contacts">
+            <span>
+              <HiOutlineMailOpen />
+            </span>
+            : <span>www.help@ecommerceapp.com</span>
+          </p>
+          <p className="contacts">
+            <span>
+              <FiPhoneCall />
+            </span>{" "}
+            : <span>012-3456789</span>
+          </p>
+          <p className="contacts">
+            <span>
+              <MdHeadsetMic />
+            </span>{" "}
+            :<span>1800-0000-0000 (toll-free)</span>
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
