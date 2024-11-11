2c50fb3c0336490b2bb0f2261408c98f9754a0aa
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-11 17:40:54 +0530

    :zap: Page Not found :art: Footer, Page Not Found :package: react-router-dom

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,8 +1,57 @@
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <h1>Header</h1>
-  )
-}
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand" to="/">
+              🛒 E-commerce brand
+            </Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/category">
+                  Category
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/cart">
+                  Cart {0}
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Header
+export default Header;
--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 
 const PageNotFound = () => {
   return (
     <Layout>
-      <h1>PageNotFound</h1>
+      <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oopss ! Page Not Found</h2>
+        <Link to="/" className="pnf-button">
+          Go Back
+        </Link>
+      </div>
     </Layout>
   );
 };
