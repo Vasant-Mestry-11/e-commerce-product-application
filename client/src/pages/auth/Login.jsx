6ce421da12f7bd8c82944286d9c1d32232cc4fa2
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-18 22:31:40 +0530

    :zap: :lipstick: Login page

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,7 +1,86 @@
+import { useState } from "react";
 import Layout from "../../components/Layout/Layout";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <Layout title="Login">Login</Layout>;
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+    address: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUserDetails((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`http://localhost:8080/api/v1/auth/login`, {
+        email,
+        password,
+      });
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  const { email, password } = userDetails;
+  return (
+    <Layout title="Login">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h1 className="title">Login</h1>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail"
+              placeholder="Enter email"
+              value={email}
+              name="email"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Password"
+              value={password}
+              name="password"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
 };
 
 export default Login;
