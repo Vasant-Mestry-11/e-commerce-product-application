274010e54ee9975fb6f011d97545df616e4f5c49
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-19 20:55:51 +0530

    :zap: :flags: useAuth Context

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,15 @@
-import Layout from "../components/Layout/Layout"
+import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
 const Homepage = () => {
+  const [auth, setAuth] = useAuth();
+
   return (
     <Layout title="Best offers">
       <h1>Home</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
