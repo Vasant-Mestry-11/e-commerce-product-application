fc9fe14ddbcd2405f044a7ec6421c2a4f4dd8fbb
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-20 21:21:54 +0530

    :zap: :art: Added spinner, Protected routes

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Spinner = () => {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => --prevCount);
+    }, 1000);
+
+    if (count === 0) {
+      navigate("/login");
+    }
+
+    return () => clearInterval(timer);
+  }, [navigate, count]);
+
+  return (
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <h1>Redirecting to you in {count} seconds</h1>
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
--- client/src/providers/authProvider.jsx
+++ client/src/providers/authProvider.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import { AuthContext } from "../context/authContext";
+import axios from "axios";
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({
     user: null,
     token: "",
   });
+
+  axios.defaults.headers.common["Authorization"] = auth?.token;
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("auth"));
